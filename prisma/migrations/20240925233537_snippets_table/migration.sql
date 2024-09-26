c990f3eb7e152681b6c5786c88b0f97c43a9106e
renatorrocha <renatorrodrigues2002@gmail.com>
2024-09-25 21:41:39 -0300

    feat(db): migration and prismaClient

---
+++ prisma/migrations/20240925233537_snippets_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Snippet" (
+    "id" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Snippet_pkey" PRIMARY KEY ("id")
+);
