cb350ddb0c308bf4bf45961e5c3912300728b683
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-10-02 08:43:51 -0300

    docs: tags

--- src/routes/snippet-routes.ts
+++ src/routes/snippet-routes.ts
@@ -2,17 +2,21 @@ import Elysia from "elysia";
 import { snippetService } from "../services/snippet-service";
 import { snippetSchema } from "../schemas/snippet-schema";
 
-export const snippetRoutes = new Elysia().group("/snippets", (app) =>
-	app
-		.get("/", () => snippetService.getAll())
-		.get("/:id", ({ params }) => snippetService.getById(params.id))
-		.post("/", async ({ body }) => snippetService.create(body), {
-			body: snippetSchema,
-		})
-		.put(
-			"/:id",
-			async ({ body, params }) => snippetService.update(params.id, body),
-			{ body: snippetSchema },
-		)
-		.delete("/:id", async ({ params }) => snippetService.delete(params.id)),
+export const snippetRoutes = new Elysia({
+    detail: {
+        tags: ["snippets"],
+    },
+}).group("/snippets", (app) =>
+    app
+        .get("/", () => snippetService.getAll())
+        .get("/:id", ({ params }) => snippetService.getById(params.id))
+        .post("/", async ({ body }) => snippetService.create(body), {
+            body: snippetSchema,
+        })
+        .put(
+            "/:id",
+            async ({ body, params }) => snippetService.update(params.id, body),
+            { body: snippetSchema }
+        )
+        .delete("/:id", async ({ params }) => snippetService.delete(params.id))
 );
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,25 @@ import { Elysia } from "elysia";
 import { snippetRoutes } from "./routes/snippet-routes";
 import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().use(swagger()).use(snippetRoutes).listen(3000);
+const app = new Elysia()
+    .use(
+        swagger({
+            documentation: {
+                info: {
+                    title: "CRUD Snippet Documentation",
+                    version: "1.0.0",
+                },
+                tags: [
+                    {
+                        name: "snippets",
+                        description: "Snippets endpoints",
+                    },
+                ],
+            },
+        })
+    )
+    .use(snippetRoutes)
+    .listen(3000);
 
 console.log(
     `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
